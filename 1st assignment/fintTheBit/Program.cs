6f75bfd306f1cda71c7d03309d60b42ce656bd9f
ahmedakef <you@example.com>
2017-10-22 22:53:03 +0200

    start the second project

---
+++ 1st assignment/fintTheBit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fintTheBit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
