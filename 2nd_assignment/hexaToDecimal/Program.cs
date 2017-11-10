89d7d79655b87ab9acccb4f6f9ba99ecc5d132e3
ahmedakef <you@example.com>
2017-11-10 23:30:03 +0200

    solved 2nd assignment

---
+++ 2nd_assignment/hexaToDecimal/Program.cs
@@ -0,0 +1,30 @@
+﻿/* by :   ahmed hamed osman akef  */
+using System;
+
+namespace hexaToDecimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter the number in hexaDecimal");
+            string hexa = Console.ReadLine();
+            
+            hexa = hexa.ToUpper();
+            int i =1,digit, dec=0;
+            int[] values = { 10,11,12,13,14,15 };
+            for(int j=hexa.Length -1 ; j >= 0 ; j--){
+
+                digit = Convert.ToInt32(char.GetNumericValue(hexa[j])); 
+                
+                if(digit == -1){
+                    digit = values[(int) hexa[j]-'A'];
+                }
+                dec += digit * i;
+                i*=16; 
+            }
+
+            Console.WriteLine("the decimal is : " + dec );
+        }
+    }
+}
