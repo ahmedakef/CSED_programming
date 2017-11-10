779df0b95f80652843802dbea4bf07a35c1fe424
ahmedakef <you@example.com>
2017-11-11 00:37:35 +0200

    solve 3rd assignment

---
+++ 2nd_lab/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _2nd_lab
+{
+    class Program
+    {
+        public static void alpha(string name){
+            for(int i=0 ; i<name.Length ; i++){
+                if(name[i]==' ')
+                    continue;
+                Console.Write(name[i]-(int)'a');
+                Console.Write(' ');
+            }
+            
+
+        }
+        public static int avr_arr(params int[]  arr){
+            int sum=0;
+            for(int i=0 ; i<arr.Length ; i++){
+                sum += arr[i];
+            }
+
+            return sum/arr.Length;
+
+        } 
+
+        public static int biggest(int fst,int snd ){
+            int max = fst>snd ?fst : snd;
+
+            return max;
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n the first program");
+            alpha("ahmed akef");
+            
+            int avvr = avr_arr(5,3,2);
+            Console.WriteLine("\n the second program");
+            Console.WriteLine(avvr);
+
+            int bigg = biggest(5,3);
+            bigg = biggest(bigg,8);
+            Console.WriteLine("\n the third program");
+            Console.WriteLine(bigg);
+        }
+    }
+}
---
+++ 3rd_assignment/studentsDregrees/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace studentsDregrees
+{
+    class Program
+    {
+        string[] names = {"ahmed","ali","mona","khaled","radwan","balha"};
+        int[]  degrees = {50,45,40,35,30,5};
+        public int get_degree(){
+            Console.WriteLine("Enter student name");
+            string student_name = Console.ReadLine();
+            int idx = Array.IndexOf(names,student_name);
+            if( idx != -1 ){
+                return degrees[idx];
+            }else{
+                Console.WriteLine("Enter correct name");
+            }
+            return 0;
+            
+        }
+        public void Top_student(){
+            Console.WriteLine("top student is : {0} and his degree is {1} " , names[0] , degrees[0] );
+        }
+        public void chane_name(){
+            Console.WriteLine("Enter student name");
+            string student_name = Console.ReadLine();
+            int idx = Array.IndexOf(names,student_name) ;
+            if( idx != -1 ){
+                Console.WriteLine("Enter new name");
+                string new_name = Console.ReadLine();
+                names[idx] = new_name;
+            }else{
+                Console.WriteLine("Enter correct name");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            
+            
+            Console.WriteLine("Please type a number : ");
+            Console.WriteLine("1) search for student degree ");
+            Console.WriteLine("2) get top student name and degree ");
+            Console.WriteLine("3) change student name ");
+
+            int ch = Convert.ToInt32(Console.ReadLine());
+
+            Program p = new Program();
+
+            if(ch==1){
+                int degree = p.get_degree();
+                Console.WriteLine(degree);
+            }else if(ch==2){
+                p.Top_student();
+            }else if(ch==3){
+                p.chane_name();
+            }else{
+                Console.WriteLine("choose correct number");
+            }
+        }
+    }
+}
