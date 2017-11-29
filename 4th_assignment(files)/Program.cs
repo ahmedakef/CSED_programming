a47104b90f7657689d39092e1e405b5a3d3f0195
ahmedakef <you@example.com>
2017-11-29 10:38:43 +0200

    test programs

--- 4th_assignment(files)/Program.cs
+++ 4th_assignment(files)/Program.cs
@@ -14,7 +14,7 @@ namespace _4th_assignment_files_
             this.name = name;
         }
         public int rename(string new_name){
-            bool same_name_in_dir = true ; // ToDo  we can call OS to jnow that
+            bool same_name_in_dir = false ; // ToDo  we can call OS to jnow that
             if( !same_name_in_dir){
                 this.name = new_name;
                 Console.WriteLine("file renamed");
@@ -27,6 +27,10 @@ namespace _4th_assignment_files_
             //To Do
             // OS.delete ( this.name );   // it will be some thinng like that
             Console.WriteLine("file deleted");
+        }
+        public string get_name(){
+            
+            return this.name;
         }
         virtual public void open(){
             Console.WriteLine("file is openned");
@@ -79,10 +83,19 @@ namespace _4th_assignment_files_
         static void Main(string[] args)
         {
             File file1 = new File("file1");
-            Text text1 = new Text("text1");
-            Image image1= new Image("image1");
-            Video Video = new Video("video1");
+            file1.open();
+            file1.rename("ahmed");
+            Console.WriteLine(file1.get_name());
 
+            Text text1 = new Text("text1");
+            text1.open();
+            Image image1= new Image("image1");
+            image1.open();
+            Audio audio1 = new Audio("audio1");
+            audio1.play();
+            Video video1 = new Video("video1");
+            video1.rename("hoda");
+            Console.WriteLine(video1.get_name());
         }
     }
 }
